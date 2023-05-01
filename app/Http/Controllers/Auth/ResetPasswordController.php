22965e5a09e20854b4338a54051e235d79f0a96d
David1A63 <despanime@gmail.com>
2023-04-30 21:07:07 -0600

    Traduciendo los mensajes de validacion de ResetPasswords

--- app/Http/Controllers/Auth/ResetPasswordController.php
+++ app/Http/Controllers/Auth/ResetPasswordController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
 use App\Providers\RouteServiceProvider;
+//Aquí se hacen los procesos para resetear la contraseña
 use Illuminate\Foundation\Auth\ResetsPasswords;
 
 class ResetPasswordController extends Controller
